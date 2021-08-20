986fd8ce2fe13e2b3af9be24361321e992ec1ce7
Egorov Alexander <egorovan2@student.bmstu.ru>
2021-08-20 04:43:56 +0300

    add content from Habr

--- Parsers/XmlParser.cs
+++ Parsers/XmlParser.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using rss_feeder.Interfaces;
 using rss_feeder.Models;
+using System.Xml.Linq;
 
 namespace rss_feeder.Parsers
 {
@@ -12,6 +13,20 @@ namespace rss_feeder.Parsers
         public IEnumerable<Item> Parse(string src_text)
         {
             List<Item> items = new List<Item>();
+            XDocument xmlDoc = XDocument.Load(src_text);
+            XElement xRss = xmlDoc.Element("rss");
+            XElement xChannel = xRss.Element("channel");
+            foreach (XElement xItem  in xChannel.Elements("item").ToList())
+            {
+                Item item = new Item();
+                item.Title = xItem.Element("title").Value;
+                item.Description = xItem.Element("description").Value;
+                item.PubDate = DateTime.Parse(xItem.Element("pubDate").Value);
+                item.PubDate =  item.PubDate.AddHours(-3);
+                item.Url = xItem.Element("guid").Value;
+                items.Add(item);
+            }
+
             return items;
         }
     }
